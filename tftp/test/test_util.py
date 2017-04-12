7020a0ac56e48fd5351dc4099a2a555cdf901c72
Gavin Panella <gavin@gromper.net>
2017-04-12 10:48:26 +0200

    Test iterlast directly.

--- tftp/test/test_util.py
+++ tftp/test/test_util.py
@@ -1,7 +1,9 @@
 '''
 @author: shylent
 '''
-from tftp.util import CANCELLED, timedCaller
+from itertools import count, islice
+from random import randint
+from tftp.util import CANCELLED, iterlast, timedCaller
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import Clock
 from twisted.trial import unittest
@@ -61,3 +63,33 @@ class TimedCaller(unittest.TestCase):
         # Advancing the clock does not result in more calls.
         clock.advance(3)
         self.assertEqual(["call", "call"], record)
+
+
+class IterLast(unittest.TestCase):
+
+    def test_yields_nothing_when_no_input(self):
+        self.assertEqual([], list(iterlast([])))
+
+    def test_yields_once_for_input_of_one(self):
+        thing = object()
+        self.assertEqual(
+            [(True, thing)],
+            list(iterlast([thing])))
+
+    def test_yields_once_for_each_input(self):
+        things = [object() for _ in range(1, randint(9, 99))]
+        self.assertEqual(
+            [(False, thing) for thing in things[:-1]] + [(True, things[-1])],
+            list(iterlast(things)))
+
+    def test_works_with_unsized_iterators(self):
+        things = [object() for _ in range(1, randint(9, 99))]
+        self.assertEqual(
+            [(False, thing) for thing in things[:-1]] + [(True, things[-1])],
+            list(iterlast(iter(things))))
+
+    def test_works_with_infinite_iterators(self):
+        # ... though is_last will never be True.
+        self.assertEqual(
+            [(False, 1), (False, 2), (False, 3)],
+            list(islice(iterlast(count(1)), 3)))
